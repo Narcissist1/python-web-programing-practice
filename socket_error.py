81ec29d3aa8278021f90f015dfe1748edc84de40
Narcissist1 <m6106918@gmail.com>
2015-06-05 19:48:24 +0800

    add sokect error handle example

---
+++ socket_error.py
@@ -0,0 +1,10 @@
+import sys
+import socket
+import argparse
+
+def main():
+	#setup argument parsing
+	parser=argparse.ArgumentParser(description='Socket Error Examples')
+	parser.add_argument('--host',action="store",dest="host",required=False)
+	parser.add_argument('--port',action="store",dest="port",type=int,required=False)
+	
